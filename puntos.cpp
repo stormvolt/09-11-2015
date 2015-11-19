575952e27f3e72161a2bb88cf1c4e538da59e97e
stormvolt <luisnu97@hotmail.com>
2015-11-19 09:29:10 -0500

    Update puntos.cpp

--- puntos.cpp
+++ puntos.cpp
@@ -1,18 +1,50 @@
 #include <iostream> 
+#include <cmath>
 using namespace std;
- 
-template <typename T>
-class Point {
+
+template<class type> class Vector;
+template <class type>
+
+class Point
+{
 	private:
-		T x, y;
+		type x, y;
 	public:
-		Point(const T u, const T v) : x(u), y(v) {}
-		T getX() { return x; }
-		T getY() { return y; }
+	    Point(){}
+		Point( type u, type v) : x(u), y(v) {}
+	friend class Vector<type>;
+	
+};
+
+template <class type> class Vector
+{
+    private:
+            Point<type> start;
+            Point<type> end;
+    public:
+            Vector() {};
+           
+            void add(Point<type> a, Point<type> b)
+            {
+                start=a;
+                end=b;
+            }
+            
+            double distance()
+            {
+	            return sqrt((end.x-start.x)*(end.x-start.x) + (end.y-start.y)*(end.y-start.y));
+            }
 };
  
+
+        
+
+ 
 int main() {
-	Point<float> fpoint(2.5, 3.5);
-	cout << fpoint.getX() << ", " << fpoint.getY() << endl;
+	Point<float> x(2.5, 3.5);
+	Point<float> y(1.5, 4.5);
+	Vector<float> fVector;
+	fVector.add(x,y);
+	cout<< fVector.distance();
 	return 0;
 }
